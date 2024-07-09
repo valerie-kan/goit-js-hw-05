cff9a26b01bddd4261c3bb68c150d092b6da459b
Valeriia <lera_kanavchenko@ukr.net>
2024-07-09 20:29:44 +0300

    Add arrow functions

---
+++ js/task-4.js
@@ -0,0 +1,51 @@
+const getTotalBalanceByGender = (users, gender) => {
+    const genderUsers = users.filter(user => user.gender === gender);
+    let balance = 0; 
+    for (const user of genderUsers) {
+        balance += user.balance;
+    }
+    return balance;
+}
+
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
